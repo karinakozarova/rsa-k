344ec6169761b86a3a34006b5de65ce1c1d9b4e3
karinakozarova <karinakozarova1303@gmail.com>
2018-01-09 13:59:32 +0200

    added test structure

---
+++ spec/controllers/decrypt_controller_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe DecryptMessagesController, type: :controller do
+	describe "POST /rsas/:id/decrypt_messages/" , :type => :request do
+		it  do
+			
+		end
+	end
+end
---
+++ spec/controllers/encrypt_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe EncryptMessagesController, type: :controller do
+
+	describe "GET /rsas/:id/encrypt_messages/:id", :type => :request do
+		it  do
+			
+		end		
+	end
+
+	describe "POST /rsas/:id/encrypt_messages/" , :type => :request do
+		it  do
+			
+		end
+	end
+
+
+
+end
